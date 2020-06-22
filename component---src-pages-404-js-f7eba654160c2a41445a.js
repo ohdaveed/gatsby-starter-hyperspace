b431e9aef96fff776030d3e73e20e824a95c5710
David Arrizon <arrizon.david@gmail.com>
2020-06-22 07:51:58 -0500

    Updates

---
+++ component---src-pages-404-js-f7eba654160c2a41445a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),c=t("JfNc");n.default=function(){return l.a.createElement(c.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"Not a valid URL"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f7eba654160c2a41445a.js.map
